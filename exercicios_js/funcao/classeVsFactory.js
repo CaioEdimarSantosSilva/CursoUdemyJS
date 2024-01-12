e2adeab1140e807d78e08a47e281c96752795a6f
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-12 09:14:53 -0300

    Funções

---
+++ exercicios_js/funcao/classeVsFactory.js
@@ -0,0 +1,18 @@
+class Pessoa {
+    constructor(nome){
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('João')
+p2.falar()
