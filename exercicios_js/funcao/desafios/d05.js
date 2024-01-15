9bb8d4849b757de4f3797c90e5b14d283a41d2ba
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-15 10:20:12 -0300

    Create d05.js

---
+++ exercicios_js/funcao/desafios/d05.js
@@ -0,0 +1,7 @@
+function moeda(valor=1){
+    console.log(`Real = R$${valor.toFixed(2).replace('.',',')}`)
+    console.log(`Euro = €${valor.toFixed(2).replace('.',',')}`)
+    console.log(`Dólar = $${valor.toFixed(2).replace('.',',')}`)
+    
+}
+moeda(53.934)
