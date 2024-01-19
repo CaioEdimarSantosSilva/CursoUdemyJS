1d9db95457b99745cb3338bd406eb0d9aba391da
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-19 10:03:20 -0300

    Create d15.js

---
+++ exercicios_js/funcao/desafios/d15.js
@@ -0,0 +1,24 @@
+/**Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda
+possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o
+comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza
+que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console:
+“Não trabalhamos com este tipo de automóvel aqui”. */
+
+function carros(carro){
+        switch (carro) {
+        case 'hatch':
+            console.log(`Compra do carro ${carro} efetuada com sucesso`)
+            break
+        case 'sedans': case 'motocicletas': case 'caminhonetes':
+            console.log(`Tem certeza que não prefere este modelo? ${carro}`)
+            break
+        default:
+            console.log(`Não trabalhamos com ${carro}`)
+            break
+    }
+}
+carros('hatch')
+carros('sedans')
+carros('motocicletas')
+carros('caminhonetes')
+carros('fusca')
