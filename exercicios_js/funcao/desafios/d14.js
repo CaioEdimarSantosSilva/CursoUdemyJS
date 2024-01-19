6efe3dd906a3c8a516322b9a3a6f53a0860cfe42
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-19 10:03:17 -0300

    Create d14.js

---
+++ exercicios_js/funcao/desafios/d14.js
@@ -0,0 +1,21 @@
+function frutas(fruta = '') {
+    switch (fruta.toLowerCase()) {
+        case 'maçã':
+            console.log(`Não vendemos ${fruta} aqui.`)
+            break
+        case 'kiwi':
+            console.log(`Estamos com escassez de ${fruta}.`)
+            break
+        case 'melancia':
+            console.log(`Aqui a ${fruta} está, são 3 reais o quilo.`)
+            break
+        default:
+            console.log(TypeError(`Erro, a ${fruta} não tem.`))
+            break
+    }
+}
+
+frutas('maçã')
+frutas('kiwi')
+frutas('melancia')
+frutas('banana')
