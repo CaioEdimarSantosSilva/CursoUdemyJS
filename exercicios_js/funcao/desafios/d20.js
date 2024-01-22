dbc367ed0994d3e8c000ab5f4a3efcd75a1b40ed
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-22 11:02:12 -0300

    Create d20.js

---
+++ exercicios_js/funcao/desafios/d20.js
@@ -0,0 +1,44 @@
+/**Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas
+para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1.
+Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, ao solicitar R$18, o programa deve
+informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1
+nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1. 
+let cedula = {}
+function cedulas(valor) {*/
+
+let cedula = { 100: 0, 50: 0, 10: 0, 5: 0, 1: 0 }
+
+function cedulas(valor) {
+    console.log(`Total: R$${(valor).toFixed(2).replace('.', ',')}`)
+    while (valor !== 0) {
+        if (valor >= 100) {
+            cedula[100]++
+            valor -= 100
+
+        } else if (valor >= 50) {
+            cedula[50]++
+            valor -= 50
+
+        } else if (valor >= 10) {
+            cedula[10]++
+            valor -= 10
+
+        } else if (valor >= 5) {
+            cedula[5]++
+            valor -= 5
+
+        } else if (valor >= 1) {
+            cedula[1]++
+            valor -= 1
+
+        }
+    }
+    console.log('VOCÊ SACOU:')
+    for (let nota in cedula) {
+        if (cedula[nota] !== 0) {
+            console.log(`${cedula[nota]} nota(s) de R$ ${parseInt(nota).toFixed(2).replace('.', ',')}`)
+        }
+    }
+}
+cedulas(196)
+
