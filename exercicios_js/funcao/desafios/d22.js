4bbf32d1e3c81a85b713697c3993037d4d2265df
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-24 09:58:46 -0300

    Create d22.js

---
+++ exercicios_js/funcao/desafios/d22.js
@@ -0,0 +1,17 @@
+/**Criar uma função para calcular o valor a ser pago de anuidade de uma associação. A função recebe como
+parâmetro um inteiro que representa o mês (1 - janeiro, 2 - fevereiro…) que foi paga e o valor da anuidade. A
+anuidade deve ser paga no mês de janeiro. Por mês, é cobrado 5% de juros (sob o regime de juros
+compostos). O retorno deve ser o valor a ser pago para o respectivo mês escolhido.
+ */
+
+function anuidade(mes,aluguel){
+    for(let indice = 1; indice <= mes; indice++){
+        valorTotal =  0.05 * aluguel
+        aluguel += valorTotal
+    }
+    console.log(`Do mês 1 até o mês ${mes} o valor total com juros é R$${parseInt(aluguel).toFixed(2).replace('.',',')}`)
+}
+
+anuidade(100,800)
+anuidade(7,1000)
+anuidade(2,50)
