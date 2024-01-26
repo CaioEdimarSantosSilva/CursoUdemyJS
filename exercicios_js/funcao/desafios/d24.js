517dbae6238ff00ada68a35cc3ca379921e85a74
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-26 10:20:15 -0300

    Create d24.js

---
+++ exercicios_js/funcao/desafios/d24.js
@@ -0,0 +1,6 @@
+// Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+let contador = 1
+while(contador <= 11){
+    console.log(`Contando: ${contador}`)
+    contador++
+}
