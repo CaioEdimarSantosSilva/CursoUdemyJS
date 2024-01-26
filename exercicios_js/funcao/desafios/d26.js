72e8a55f487654972d868340cbb13a28430ace0f
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-26 10:20:19 -0300

    Create d26.js

---
+++ exercicios_js/funcao/desafios/d26.js
@@ -0,0 +1,7 @@
+for(let contador = 1; contador <= 100; contador++){
+    if(contador % 2 === 0){
+        console.log(`Esse numero é PAR:${contador}`)
+    }else{
+        console.log(`Esse numero é IMPAR:${contador}`)
+    }
+}
