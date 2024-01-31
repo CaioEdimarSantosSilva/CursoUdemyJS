8888a9c53e68f0a5aca10bb4a063d062e2f8be9c
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-31 10:47:18 -0300

    Create ex001.js

---
+++ exercicios_js/exerciciosChatGpt/ex001.js
@@ -0,0 +1,7 @@
+/**Crie uma arrow function que receba dois números e retorne a soma deles. */
+
+const soma = (valor1, valor2) =>{
+    return valor1 + valor2
+}
+
+console.log(`A soma dos valores é ${soma(5, 2)}`)
