b6680ee4483bcf0a062ba5fd42e51fc64b278185
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-31 12:35:58 -0300

    Update ex004.js

--- exercicios_js/exerciciosChatGpt/ex004.js
+++ exercicios_js/exerciciosChatGpt/ex004.js
@@ -1,2 +1,6 @@
 /**Desenvolva uma arrow function que calcule e retorne o quadrado de um número. */
+const quadrado = (numero) => {
+    return numero * numero
+}
 
+console.log(`O quadrado desse numero é ${quadrado(7)}!`)
