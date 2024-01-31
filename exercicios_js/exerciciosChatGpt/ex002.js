6d42970cf006fa2e898ec37970f379699a0db46c
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-31 10:47:21 -0300

    Create ex002.js

---
+++ exercicios_js/exerciciosChatGpt/ex002.js
@@ -0,0 +1,7 @@
+/**Crie uma arrow function que calcule e retorne o dobro de um número. */
+
+const dobro = (numero) => {
+    return numero * 2
+}
+
+console.log(`O dobro desse numero é ${dobro(7)}!`)
