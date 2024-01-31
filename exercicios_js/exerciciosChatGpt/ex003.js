39101496c983dce6b300ea2d928fbde4ced249be
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-31 10:47:24 -0300

    Create ex003.js

---
+++ exercicios_js/exerciciosChatGpt/ex003.js
@@ -0,0 +1,6 @@
+/**Escreva uma arrow function que receba três números como parâmetros e retorne a média deles. */
+
+const media = (valor1, valor2, valor3) => {
+    return (valor1 + valor2 + valor3) / 3
+}
+console.log(media(5, 5, 5))
