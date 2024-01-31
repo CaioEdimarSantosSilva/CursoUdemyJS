ba6560b62d6b4e689169e1f2cd63805961e3c31d
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-01-31 14:05:51 -0300

    exercicios

---
+++ exercicios_js/exerciciosChatGpt/ex010.js
@@ -0,0 +1,7 @@
+/**Desenvolva uma arrow function que receba dois valores e retorne
+o resto da divisão do primeiro pelo segundo.*/
+
+const restoDaDvisao = (valor1, valor2) => {
+    return valor1 % valor2
+}
+console.log(`O resto da divisão é ${restoDaDvisao(8, 2)}`)
