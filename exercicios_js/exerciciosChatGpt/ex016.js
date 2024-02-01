2c018fa2f178169c76f1578586fc1421b58ce531
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-01 10:30:41 -0300

    Create ex016.js

---
+++ exercicios_js/exerciciosChatGpt/ex016.js
@@ -0,0 +1,7 @@
+/**Escreva uma arrow function que receba um objeto com informações de uma pessoa 
+(nome, idade, sexo) e retorne uma string no formato "Nome: [nome], Idade: [idade], Sexo: [sexo]".*/
+
+const pessoa = { nome: 'Lorax', idade: 24, sexo: 'Feminino' }
+const info = cliente => string = JSON.stringify(cliente)
+info(pessoa)
+console.log(string)
