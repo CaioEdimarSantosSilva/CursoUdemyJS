1e3eaec757e631a48219c5a82da8a25e1173ce2a
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-01 16:18:38 -0300

    Update ex012.js

--- exercicios_js/exerciciosChatGpt/ex012.js
+++ exercicios_js/exerciciosChatGpt/ex012.js
@@ -2,13 +2,17 @@
 retorne true se ele for primo, false caso contrário.*/
 
 const numeroPrimo = (valor) => {
-    console.log(`O numero ${valor} se for primo é verdadeiro`)
-    if(valor % 2 != 0 && valor % 3 != 0 && valor % 5 != 0 && valor % 7 != 0){
-        return true
+
+    if (valor % 2 != 0 && valor % 3 != 0 && valor % 5 != 0 && valor % 7 != 0) {
+        console.log(`${valor} é um número primo.`)
+        return true 
     }
-    else{
+    else {
+        console.log(`${valor} não é um número primo.`)
         return false
     }
 }
 
 console.log(numeroPrimo(11))
+
+
