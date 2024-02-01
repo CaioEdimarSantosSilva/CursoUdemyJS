b1581b29cb43ccc0cf9a3d869fb3ac48e310fce7
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-01 16:18:46 -0300

    Update ex016.js

--- exercicios_js/exerciciosChatGpt/ex016.js
+++ exercicios_js/exerciciosChatGpt/ex016.js
@@ -2,6 +2,7 @@
 (nome, idade, sexo) e retorne uma string no formato "Nome: [nome], Idade: [idade], Sexo: [sexo]".*/
 
 const pessoa = { nome: 'Lorax', idade: 24, sexo: 'Feminino' }
-const info = cliente => string = JSON.stringify(cliente)
-info(pessoa)
-console.log(string)
+console.log('Objeto',pessoa)
+const info = cliente => string = `String "Nome: [${pessoa.nome}], Idade: [${pessoa.idade}], Sexo: [${pessoa.sexo}]"`
+
+console.log(info(pessoa))
