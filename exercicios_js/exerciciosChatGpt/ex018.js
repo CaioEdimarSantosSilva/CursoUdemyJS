1e15b79147235b5dd796819abc7688bcfee57841
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-01 16:19:11 -0300

    Create ex018.js

---
+++ exercicios_js/exerciciosChatGpt/ex018.js
@@ -0,0 +1,5 @@
+/**Desenvolva uma arrow function que receba uma temperatura em Celsius
+e retorne a temperatura equivalente em Fahrenheit.*/
+
+const temperatura = celsius => fahrenheit = (celsius * 9/5) + 32
+console.log(`A temperatura de ${celsius = 1}°C celsius em fahrenheit é ${temperatura(celsius)}°F`)
