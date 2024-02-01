d357c57eace299b90d80781086c15cace0f71113
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-01 10:30:33 -0300

    Create ex013.js

---
+++ exercicios_js/exerciciosChatGpt/ex013.js
@@ -0,0 +1,3 @@
+/**Escreva uma função que receba uma string e retorne o número de caracteres presentes nela */
+const tamanho = string  => string.length
+console.log(`A string tem como tamanho ${tamanho('Lorax')} caracteres.`)
