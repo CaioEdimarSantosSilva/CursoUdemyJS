d35f74ff2cd7307d63cd0f6741db3d3ad973c191
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-01 16:18:42 -0300

    Update ex014.js

--- exercicios_js/exerciciosChatGpt/ex014.js
+++ exercicios_js/exerciciosChatGpt/ex014.js
@@ -5,6 +5,8 @@ const lista1 = [1, 2, 3, 4, 6]
 const lista2 = [1, 3, 4, 5, 6]
 const lista3 = []
 const emComum = (array1, array2) => {
+    console.log(`A primeira lista é [${lista1.join(', ')}]`)
+    console.log(`A segunda lista é [${lista2.join(', ')}]`)
     for (indice1 in array1) {
         for (indice2 in array2) {
             if (array1[indice1] === array2[indice2]) {
@@ -12,6 +14,6 @@ const emComum = (array1, array2) => {
             }
         }
     }
-    console.log(lista3)
+    console.log(`A terceira lista com os elemetos em comum é [${lista3.join(', ')}]`)
 }
 emComum(lista1, lista2)
