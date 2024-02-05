3e0a4ac1119869e207819994aac4cca1a23cb39d
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-05 18:00:55 -0300

    Update d07.js

--- exercicios_js/funcao/desafios/d07.js
+++ exercicios_js/funcao/desafios/d07.js
@@ -2,13 +2,13 @@ function Bhaskara(a, b, c){
     const delta = b**2 -4 * a * c
     console.log(`O delta é ${delta}`)
     if (delta >= 0) {
-        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a)
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a)
     
-        console.log(`[${x1}, ${x2}]`);
+        console.log(`[${x1}, ${x2}]`)
         
     } else {
-        console.log("Delta Negativo");
+        console.log("Delta Negativo")
     }
 
 }
