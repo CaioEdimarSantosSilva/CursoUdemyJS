7fc10b4d67d1f4c535a8158c9897474f97577610
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-05 18:00:54 -0300

    Update d02.js

--- exercicios_js/funcao/desafios/d02.js
+++ exercicios_js/funcao/desafios/d02.js
@@ -1,17 +1,16 @@
 function triangulos(n1 = 1, n2 = 1, n3 = 1) {
     if (n1 == n2 && n2 == n3) {
         console.log(`O triangulo com os lados ${n1}, ${n2} e ${n3} é Triangulo Equilátero`)
-    }
-    if (n1 == n2 || n2 == n3) {
+    } else if (n1 == n2 || n2 == n3) {
         if (n1 != n2 || n2 != n3) {
             console.log(`O triangulo com os lados ${n1}, ${n2} e ${n3} é Triangulo Isósceles`)
         }
     }
-    if (n1 != n2 && n2 != n3) {
+    else if (n1 != n2 && n2 != n3) {
         console.log(`O triangulo com os lados ${n1}, ${n2} e ${n3} é Triangulo Escaleno`)
     }
 
 }
-triangulos(2, 2, 2) 
-triangulos(2, 3, 3) 
+triangulos(2, 2, 2)
+triangulos(2, 3, 3)
 triangulos(2, 3, 4) 
