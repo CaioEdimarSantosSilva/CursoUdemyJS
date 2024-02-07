2ca6d6ad92d85a01426d165dd2b9f9baa7f2fa10
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-07 09:48:26 -0300

    Create oo.js

---
+++ exercicios_js/objeto/oo.js
@@ -0,0 +1,21 @@
+// Código não executavel!!!
+
+//Procedural
+processamento(valor1, valor2)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+//Principios importantes:
+//1. Abstracao
+//2. Encapsuçamento
+//3. Herança (prototype)
+//4. Polimorfismo
