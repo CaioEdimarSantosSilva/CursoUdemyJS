0b6006050dd66d4216df21fb400509ec9a4e65c4
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-06 22:47:00 -0300

    Update ex034.js

--- exercicios_js/exerciciosChatGpt/ex034.js
+++ exercicios_js/exerciciosChatGpt/ex034.js
@@ -1,2 +1,10 @@
 /**Crie uma arrow function que receba um objeto representando uma pessoa com propriedades nome, sobrenome, idade e altura, e retorne true se a pessoa for maior de idade e false caso contrário */
-const pessoa = {nome: 'Caio', : }
+const pessoa = { nome: 'Vitor', sobrenome: 'Santos', idade: 27, altura: 1.67 }
+
+const maiorIdade = objeto => objeto.idade >= 18
+
+if(maiorIdade(pessoa) == true){
+    console.log(`${pessoa.nome} ${pessoa.sobrenome} é maior de idade!`)
+}else{
+    console.log(`${pessoa.nome} ${pessoa.sobrenome} não é maior de idade!`)
+}
