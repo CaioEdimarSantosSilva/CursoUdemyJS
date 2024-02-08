cd3d17ced0e3e5e668b98901b50b0660a492fa46
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-08 12:33:35 -0300

    Create ex038.js

---
+++ exercicios_js/exerciciosChatGpt/ex038.js
@@ -0,0 +1,5 @@
+/**Escreva uma arrow function que receba um objeto representando um círculo com a propriedade raio e retorne o comprimento da circunferência.
+ * C = 2πr */
+const objeto = {raio: 7}
+const prorpiedade = objt => 2 * 3.14 * objt.raio
+console.log(`A cincunferência do círculo é ${prorpiedade(objeto)}!`)
