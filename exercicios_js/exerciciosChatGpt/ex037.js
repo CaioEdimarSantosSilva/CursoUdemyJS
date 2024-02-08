c3413d9e604f28935f32d434bd24a64d7bf031ba
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-02-08 12:33:33 -0300

    Update ex037.js

--- exercicios_js/exerciciosChatGpt/ex037.js
+++ exercicios_js/exerciciosChatGpt/ex037.js
@@ -1,3 +1,5 @@
-/**Crie uma arrow function que receba um objeto representando um triângulo com as propriedades base e altura e retorne a área desse triângulo. */
+/**Crie uma arrow function que receba um objeto representando um triângulo com as propriedades base e altura e retorne a área desse triângulo. A = b*h/2, onde b é a base e h é a altura*/
 
-
+const triangulo = {base: 2, altura: 3}
+const encotraTriangulo = (objeto) => (objeto.base * objeto.altura) / 2
+console.log(`Área do triângulo com a base ${triangulo.base} e altura ${triangulo.altura} é ${encotraTriangulo(triangulo)}!`)
