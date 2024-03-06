3d35e22db04d65e4bfb6a96252730db9218e2963
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-03-06 18:20:25 -0300

    Create ex02.js

---
+++ exercicios_js/exerciciosFundamentosJS/ex02.js
@@ -0,0 +1,4 @@
+// Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias. Considere 1 ano como 365
+
+const converterIdade = ano => console.log(ano * 365)
+converterIdade(25)
