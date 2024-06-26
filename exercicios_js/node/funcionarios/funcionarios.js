def2f21b4a52de2933c6683194742a9ad847051c
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-06-26 19:26:09 -0300

    node

--- exercicios_js/node/funcionarios/funcionarios.js
+++ exercicios_js/node/funcionarios/funcionarios.js
@@ -1,14 +1,40 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
 axios.get(url).then(response => {
     const funcionarios = response.data
+    
 
+    // mulher chinesa com menor salario?
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
 
-    console.log(funcionarios)
-
+    console.log(func)
 })
 
+
+/*
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    let mulheresChinesas = []
+    for (let i = 0; i < funcionarios.length; i++) {
+        if (funcionarios[i].pais === "China" && funcionarios[i].genero === "F") {
+            mulheresChinesas.push(funcionarios[i])
+        }
+    }
+    const mulherChinesaMenorSalario = mulheresChinesas.reduce((menor, atual) => {
+        return atual.salario < menor.salario ? atual : menor
+    })
+    console.log(mulherChinesaMenorSalario)
+})
+*/
 
 
 
