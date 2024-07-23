673ba5bc098ab1cb8e90ccb539b111498338e5fc
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-07-23 18:55:52 -0300

    node

--- exercicios_js/node/projeto/src/servidor.js
+++ exercicios_js/node/projeto/src/servidor.js
@@ -2,11 +2,42 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ node: 'Notebook', preco: 123.45}) // converter para JSON
+   res.send(bancoDeDados.getProdutos()) // esse retorna a lista de objetos com os produtos
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id)) // aqui retorna cada objeto de produto dependendo do ID selecionado
+ })
+ 
+
+app.post('/produtos', (req, res, next) => { // post envia os dados
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome, 
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => { // put modifica os dados
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome, 
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => { // delete deleta os dados
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
-    console.log(`Servidor está executamdo na porta ${porta}.`)
+    console.log(`Servidor está executando na porta ${porta}`)
 })
