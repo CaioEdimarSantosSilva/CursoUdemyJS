e92cc551e1d2760a61bb0d89a80fe4e123e5bdc1
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-08-29 09:09:37 -0300

    banco de dados

---
+++ exercicios_js/banco-relacional/excluirEstados.sql
@@ -0,0 +1,9 @@
+delete from estados
+where sigla = 'T'
+
+select * from estados
+
+delete from estados
+where id >= 1000
+
+
---
+++ exercicios_js/banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,10 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+values (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+insert into estados (nome, sigla, regiao, populacao)
+values('Mais Novo', 'MN', 'Norte', 2.51)
+
+insert into estados (id, nome, sigla, regiao, populacao)
+values (28, 'Teste', 'T', 'Sul', 1.2)
+
+select * from estados
